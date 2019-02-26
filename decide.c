ba1f38166b898f27b88eb157ffdcb538773b008c
Alexander Dürr <alexander.durr@cs.lth.se>
2019-02-26 16:57:40 +0100

    feat: declare LICs
    15 boolean LICs

--- decide.c
+++ decide.c
@@ -1,5 +1,22 @@
 #include "decide.h"
 
+// declare LICs
+boolean LIC_0();
+boolean LIC_1();
+boolean LIC_2();
+boolean LIC_3();
+boolean LIC_4();
+boolean LIC_5();
+boolean LIC_6();
+boolean LIC_7();
+boolean LIC_8();
+boolean LIC_9();
+boolean LIC_10();
+boolean LIC_11();
+boolean LIC_12();
+boolean LIC_13();
+boolean LIC_15();
+
 void DECIDE(void)
 {
     
