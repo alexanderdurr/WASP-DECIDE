7d5ba21680da6f2a59aef219b5ed93aeb58a5fbc
Alexander Dürr <alexander.durr@cs.lth.se>
2019-02-26 15:19:01 +0100

    feat: basic content of decide.h
    content taken from appendix B
    some things not taken yet, to demonstrate refactoring later

--- decide.h
+++ decide.h
@@ -1,1 +1,20 @@
-// to-do
+#include <math.h>
+
+// LATER DEMONSTRATE  SOME REFACTORING
+/*
+...
+*/
+
+typedef enum { LT=1111,EQ,GT} COMPTYPE;
+
+// compares floating point numbers -- see Nonfunctional Requirements
+static inline
+COMPTYPE DOUBLECOMPARE(double A, double B)
+{
+    if (fabs(A-B)<0.000001) return EQ;
+    if (A<B) return LT;
+    return GT;
+}
+
+// function I will write
+void DECIDE(void);
