40856423cccef4f21920bcf8b65c75b01ca99348
Alexander Dürr <alexander.durr@cs.lth.se>
2019-02-26 22:09:50 +0100

    feat: calculate CMV
    according to pdf page 3 are the entries the result of the LICs

--- decide.c
+++ decide.c
@@ -160,9 +160,26 @@ boolean Area(double X1,double X2,double X3,double Y1,double Y2,double Y3,double 
 	return 0;
 } 
 
+boolean *CMV;
+
 void DECIDE(void)
 {
-    
+    // Compute Conditions Met Vektor (CMV)
+	CMV[0] = LIC_0();
+	CMV[1] = LIC_1();
+	CMV[2] = LIC_2();
+	CMV[3] = LIC_3();
+	CMV[4] = LIC_4();
+	CMV[5] = LIC_5();
+	CMV[6] = LIC_6();
+	CMV[7] = LIC_7();
+	CMV[8] = LIC_8();
+	CMV[9] = LIC_9();
+	CMV[10] = LIC_10();
+	CMV[11] = LIC_11();
+	CMV[12] = LIC_12();
+	CMV[13] = LIC_13();
+	CMV[14] = LIC_14();
 }
 
 /*
