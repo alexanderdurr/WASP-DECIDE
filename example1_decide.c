13e6f133da5567f7a99b7576cba0ea3c1171dfc1
Alexander Dürr <alexander.durr@cs.lth.se>
2019-02-26 16:34:00 +0100

    feat: example1_decide added
    placeholder for later code for example 1 in the provided document

---
+++ example1_decide.c
@@ -0,0 +1,20 @@
+// to-do
+// make an example that works on the global variables and can read the 
+/*
+
+    #include <stdio.h>
+    #include <decide.h>
+
+    int main(void){
+        // Set up a the problem in example 1 in the document
+        // ...
+
+        // run decide which changes the global variables
+        // LAUNCH, CMV, PUM, FUV
+        decide();
+
+        printf("as expected")
+        return 0;
+    }
+
+*/
