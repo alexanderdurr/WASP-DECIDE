e95d31c2e9217da1e4b23d303b4e50abec98397a
Alexander Dürr <alexander.durr@cs.lth.se>
2019-02-26 19:40:37 +0100

    feat: LIC_4 added
    Launch Interceptor Condition 4 implemented as described in the pdf

--- decide.c
+++ decide.c
@@ -40,3 +40,47 @@ boolean LIC_0()
     }
     return 0; // distance between two points smaller than LENGTH1
 }
+
+/*
+There exists at least one set of Q PTS consecutive data points that lie in more than QUADS
+quadrants. Where there is ambiguity as to which quadrant contains a given point, priority
+of decision will be by quadrant number, i.e., I, II, III, IV. For example, the data point (0,0)
+is in quadrant I, the point (-l,0) is in quadrant II, the point (0,-l) is in quadrant III, the point
+(0,1) is in quadrant I and the point (1,0) is in quadrant I.
+(2 ≤ Q PTS ≤ NUMPOINTS), (1 ≤ QUADS ≤ 3)
+*/
+boolean LIC_4()
+{
+    int i;
+    for(i=0; i<=NUMPOINTS-PARAMETERS.Q_PTS; i++)
+    {
+        Quadrant1=0;
+        Quadrant2=0;
+        Quadrant3=0;
+        Quadrant4=0;
+        for(j=0, j<PARAMETERS.Q_PTS; j++)
+        {
+             if(( (DOUBLECOMPARE(X[i+j], 0) == EQ) || (DOUBLECOMPARE(X[i+j], 0) == GT) )&& ( (DOUBLECOMPARE(Y[i+j], 0) == EQ) || (DOUBLECOMPARE(Y[i+j], 0) == GT) ))
+			{
+				Quadrant1 = 1;
+			}
+			else if(( (DOUBLECOMPARE(X[i+j], 0) == LT) ) && ( (DOUBLECOMPARE(Y[i+j], 0) == EQ) || (DOUBLECOMPARE(Y[i+j], 0) == GT) ))
+			{
+				Quadrant2 = 1;
+			}
+			else if(( (DOUBLECOMPARE(X[i+j], 0) == LT) || (DOUBLECOMPARE(X[i+j], 0) == EQ) )&& ( (DOUBLECOMPARE(Y[i+j], 0) == LT) ))
+			{
+				Quadrant3 = 1;
+			}
+			else
+			{
+				Quadrant4 = 1;
+			}
+		} 
+		if((Quadrant1+Quadrant2+Quadrant3+Quadrant4)>PARAMETERS.QUADS)
+		{
+			return 1;
+		}
+	}
+	return 0;
+} 
