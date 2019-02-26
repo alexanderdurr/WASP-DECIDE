98cd1de2299d7186852524be1a6a9c6e11f73293
Alexander Dürr <alexander.durr@cs.lth.se>
2019-02-26 21:20:31 +0100

    feat: Helper function area
    Helper function to determine if area of triangle is greater than Area

--- decide.c
+++ decide.c
@@ -524,3 +524,36 @@ boolean LIC_9()
 	}
 	return 0;
 }
+
+/*
+Helper function to determine if area of triangle is greater than Area
+*/
+boolean Area(double X1,double X2,double X3,double Y1,double Y2,double Y3,double Area)
+{
+	double sida12 = sqrt(pow(X1 - X2,2)+pow(Y1 - Y2,2));
+	double sida23 = sqrt(pow(X2 - X3,2)+pow(Y2 - Y3,2));
+	double sida13 = sqrt(pow(X1 - X3,2)+pow(Y1 - Y3,2));
+	double temp = (((sida12+sida23+sida13)/2)*(((sida12+sida23+sida13)/2)-sida12)*(((sida12+sida23+sida13)/2)-sida23)*(((sida12+sida23+sida13)/2)-sida13));
+	if((DOUBLECOMPARE(temp, -0) == EQ))
+	{
+		temp = 0;
+	}
+	double area = sqrt(temp);
+	if((DOUBLECOMPARE(((sida12+sida23+sida13)/2), sida23) == EQ)||(DOUBLECOMPARE(((sida12+sida23+sida13)/2), sida12) == EQ)||(DOUBLECOMPARE(((sida12+sida23+sida13)/2), sida13) == EQ))//This is a flat line since have the perimeter is the same size as one side
+	{
+		double area2 = (X1*(Y2-Y3))+(X2*(Y3-Y1))+(X3*(Y1-Y2));
+		area2 = area2*area2;
+		area2 = sqrt(area2);
+		area2 = .5*area2;
+		if(DOUBLECOMPARE(area2, Area) == GT)
+		{
+			return 1;
+		}
+		return 0;
+	}
+	if(DOUBLECOMPARE(area, Area) == GT)
+	{
+		return 1;
+	}
+	return 0;
+} 
