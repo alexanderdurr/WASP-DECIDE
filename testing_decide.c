775f0245442b02ce5165b609ca7f2e2f799e478f
Alexander Dürr <alexander.durr@cs.lth.se>
2019-02-28 19:08:38 +0100

    feat (testing): unit tests for LIC_11 added

--- testing_decide.c
+++ testing_decide.c
@@ -637,6 +637,57 @@ int main(void){
         printf("Test 2: failed\n");
     }
 
+    //LIC_11
+    printf("--- LIC_11 ---\n");
+    // Test Case 1
+    X[0]=0;
+    X[1]=1;
+    X[2]=2;
+    X[3]=3;
+    X[4]=4;
+    POINTS.X = X;
+    Y[0]=0;
+    Y[1]=0;
+    Y[2]=0;
+    Y[3]=0;
+    Y[4]=0;
+    POINTS.Y = Y;
+    PARAMETERS.G_PTS=1;
+    temp = LIC_11();
+    oracle = 0; // false, because X[j] - X[i] > 0 for all i < j
+    if(temp==oracle)
+    {
+        printf("Test 1: passed\n");
+    }
+    else
+    {
+        printf("Test 1: failed\n");
+    }
+    // Test Case 2
+    X[0]=0;
+    X[1]=0;
+    X[2]=1; 
+    X[3]=1;
+    X[4]=0; // one step back compared to middle
+    POINTS.X = X;
+    Y[0]=0;
+    Y[1]=0;
+    Y[2]=0;
+    Y[3]=0;
+    Y[4]=0;
+    POINTS.Y = Y;
+    PARAMETERS.G_PTS=1;
+    temp = LIC_11();
+    oracle = 1;  // true, because X[4] - X[2] < 0
+    if(temp==oracle)
+    {
+        printf("Test 2: passed\n");
+    }
+    else
+    {
+        printf("Test 2: failed\n");
+    }
+
 
     PARAMETERS.LENGTH1=1;   // Length in LICs 0, 7, 12 // DOESNT LAUNCH WHEN 2, LAUNCHES WHEN 1 <-----------------------------------EXPERIMENT HERE-----------------------------------
     PARAMETERS.RADIUS1=1;   // Radius in LICs 1, 8, 13
